87cdc823f80167caab6e7b2c0d12db93e359b5f9
Fredrik Wollsén <fredrik@neam.se>
2012-10-11 13:42:32 +0200

    Hacked away transitions (through duration = 0) between chart updates

--- nv.d3.js
+++ nv.d3.js
@@ -6053,7 +6053,7 @@ nv.models.multiBarHorizontalChart = function() {
           availableHeight = (height || parseInt(container.style('height')) || 400)
                              - margin.top - margin.bottom;
 
-      chart.update = function() { selection.transition().call(chart) };
+      chart.update = function() { selection.transition().duration(0).call(chart) };
       chart.container = this;
 
 
